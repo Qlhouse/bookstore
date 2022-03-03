d32266fd1114f2fd02ff01696f615182eef96579
john <xq127483@163.com>
2022-03-03 17:19:12 +0800

    update

---
+++ MakeProduct/fastVideoFileFPS.py
@@ -0,0 +1,11 @@
+"""
+Refer sites:
+https://pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+https://pyimagesearch.com/2017/02/06/faster-video-file-fps-with-cv2-videocapture-and-opencv/
+https://pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+https://pyimagesearch.com/2014/06/02/opencv-load-image/
+https://pyimagesearch.com/category/opencv/
+https://pyimagesearch.com/category/tutorials/
+https://pyimagesearch.com/2022/01/31/correcting-text-orientation-with-tesseract-and-python/
+https://pyimagesearch.com/2022/02/14/image-super-resolution/
+"""
