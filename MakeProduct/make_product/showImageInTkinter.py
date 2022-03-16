589a2547ba090240e444dc240453c1f0236014b7
Qlhouse <31285678+Qlhouse@users.noreply.github.com>
2022-03-16 14:54:50 +0800

    Update showImageInTkinter.py

--- MakeProduct/make_product/showImageInTkinter.py
+++ MakeProduct/make_product/showImageInTkinter.py
@@ -1,36 +1,49 @@
-import tkinter as tk
-from tkinter import PhotoImage
+from tkinter import Frame, Tk, Label, BOTH
+from PIL import Image, ImageTk
+import numpy as np
+
+class Show_Image(Frame):
+    def __init__(self, parent, imgPath):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.imgPath = imgPath
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Show Image")
+        self.pack(fill=BOTH, expand=1)
+
+        self.label1 = Label(self, border=25)
+        # self.label2 = Label(self, border=25) 
+        self.label1.grid(row=1, column=1) 
+        # self.label2.grid(row=1, column=2) 
+        self.setImage()
+ 
+
+    def setImage(self):
+        self.img = Image.open(self.imgPath)
+        self.img_resized = self.img.resize()
+        self.I = np.asarray(self.img)
+        l, h = self.img.size
+        text = str(2*l + 100) + "x" + str(h + 50) + "+0+0"
+        self.parent.geometry(text)
+        photo = ImageTk.PhotoImage(self.img)
+        self.label1.configure(image=photo)
+        self.label1.image = photo
+
+ 
+    # def onNegative(self):
+    #     # Image Negative Menu callback
+    #     I2 = 255 - self.I
+    #     img = Image.fromarray(np.uint8(I2))
+    #     photo2 = ImageTk.PhotoImage(img)
+    #     self.label2.configure(image=photo2) 
+    #     self.label2.image = photo2
 
 
-class Show_Image:
-
-    def __init__(self, root):
-        self.create_gui()
-
-    def create_gui(self):
-        self.show_image = tk.Toplevel(root)
-
-        img = PhotoImage(file="scratch.png")
-        tk.Label(root, image=img, text='Check Image:').grid(
-            row=1, column=1, sticky=tk.W, padx=15, pady=15)
-
-        tk.Button(root, text="关闭", command=self.destroy_window, bg="green", fg='white').grid(
-            row=2, column=1, padx=20, pady=20)
-
-        # self.show_image.mainloop()
-
-    # return texts for searched string and subtituded string
-    def destroy_window(self):
-        self.show_image.destroy
-
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    # print(os.getcwd())
-    root.config(background="yellow")
-    root.title('Make Products')
-    root.geometry("600x820")
-    # root.resizable(width=False, height=False)
-    application = Show_Image(root)
-    # Replace_Dialogue_Window(root)
+    
+if __name__ == "__main__":
+    root = Tk()
+    Show_Image(root, "afterRmBg2.png")
+    root.geometry("320x240")
     root.mainloop()
